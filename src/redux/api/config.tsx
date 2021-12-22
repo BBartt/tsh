6366d9ac4b72b00bf5a96bb405d49437c6a849d3
Bartosz <bartek2@onet.eu>
2021-12-22 22:33:51 +0100

    Install gh-pages, deploy on gh-pages. Add link to api in redux/api/config.tsx file, just make it work on gh-pages.

--- src/redux/api/config.tsx
+++ src/redux/api/config.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL ?? "";
-const token = process.env.REACT_APP_API_KEY ?? "";
+const BASE_URL =
+  process.env.REACT_APP_API_URL ?? "https://join-tsh-api-staging.herokuapp.com";
 
 const API = axios.create({
   baseURL: BASE_URL,
