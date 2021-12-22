55138ee1a8dccfb0bfaf24833c94d7e59f2afd89
Bartosz <bartek2@onet.eu>
2021-12-22 22:19:47 +0100

    Updates

---
+++ precache-manifest.b8cfbebda9545203925b9dd4357c978f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f139a4e6c4d1bb61d0956810f946c2",
+    "url": "/tsh/index.html"
+  },
+  {
+    "revision": "555c0ff826efabb5e6ba",
+    "url": "/tsh/static/css/main.ebf196dc.chunk.css"
+  },
+  {
+    "revision": "1b28873e037a0d6d896a",
+    "url": "/tsh/static/js/2.d578fae9.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "/tsh/static/js/2.d578fae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555c0ff826efabb5e6ba",
+    "url": "/tsh/static/js/main.4b72d1d2.chunk.js"
+  },
+  {
+    "revision": "50b3e408c7ddeb361cbf",
+    "url": "/tsh/static/js/runtime-main.5be3a166.js"
+  },
+  {
+    "revision": "5f444b61c541a4b329d8b16841b4de96",
+    "url": "/tsh/static/media/Nunito-Regular.5f444b61.ttf"
+  },
+  {
+    "revision": "ec1377dc527a55ee27635376679e6a09",
+    "url": "/tsh/static/media/Nunito-SemiBold.ec1377dc.ttf"
+  }
+]);
